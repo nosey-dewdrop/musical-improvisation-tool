d3dc99a65d4c88344e57cc9dfa0ce9dde8ac1071
nosey-dewdrop <damummyphus@gmail.com>
2025-06-30 02:37:28 +0300

    dark mode because it is dark out and my eyes are hurt (also my heart is hurt. i discovered that while person i used to love called me a hoe was loving a hoe-grande

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+    
+    if (body.classList.contains('dark-mode')) {
+        themeToggle.textContent = '☀️ Light Mode';
+    } 
+    
+    else {
+        themeToggle.textContent = '🌙 Dark Mode';
+    }
+
+});
